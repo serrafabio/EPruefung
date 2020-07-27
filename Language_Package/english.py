685e6df76f85b5ffb937c8e4c882103cff6514c5
serrafabio <37984734+serrafabio@users.noreply.github.com>
2020-07-27 11:12:10 +0200

    Add files via upload

---
+++ Language_Package/english.py
@@ -0,0 +1,101 @@
+'''
+Here we put the name of the constants of the program in english. The same file was created to the german version,
+therefore the program will have two differents languages.
+'''
+#Menu Bar Labels
+menuBar_file = 'File'
+saveAll = 'Save All'
+importBU = 'Import Back-UP'
+exportBU = 'Export Back-UP'
+send_email_to_responsible = 'Send Back-UP to the responsible'
+exit_label = 'Exit'
+menuBar_building = 'Building'
+edit_menuBar = 'Edit'
+verify_room = 'Overview of the rooms and notify co-workers'
+insertItem =  'Insert device'
+deleteItem =  'Delete device'
+addLocation = 'Add locations'
+menuBar_grossenMetrawatt = 'Gossen Metrawatt'
+importGW = 'Import Devices from Gossen Metrawatt'
+exportGW = 'Export Devices to Gossen Metrawatt'
+menuBar_help = 'Help'
+help_measure = 'Measurement instructions'
+GW_tutorial_help = 'Gossen Metrawatt tutorial'
+help_tutorial_2 = "EPrüfung instructions"
+help_tutorial_3 = 'Gossen Metrawatt Import/Export instructions'
+help_tutorial_4 = 'Automatic e-mail configurations instructions'
+menuBar_language = 'Languages'
+english = 'English'
+german = 'German'
+
+# Body Labels
+id_body = 'ID'
+devices_Body = 'Devices'
+manufacturer_body = 'Manufacturer'
+serialNumber_body = 'Serial Number'
+note_body = 'Note'
+current_Date_Body = 'Date'
+floor_body = 'Floor'
+Room_body = 'Room'
+typeRoom_body = 'Type of room:'
+office_body = 'Office'
+labor_body = 'Labor'
+insertDevice = 'INSERT DEVICE'
+deleteDevice =  'DELETE DEVICE'
+saveRoom = 'SAVE ROOM'
+floor_2_body = 'Floor at: '
+dataBank_not_compatible = 'Data Bank no Compatilble!!!'
+
+# Titles
+title_xml_import = 'Import File .xml'
+titleSearch = 'Search Results'
+
+# Dialogs
+closeBtt = 'Close'
+problemDetected = 'Problem detected!'
+problemDetected_2 = 'A problem was dected!'
+calenderName = 'Calender'
+calenderSaveOnly = 'Save just for this device'
+calenderSaveAll = 'Save for all devices in the room'
+cancelBtt = 'Cancel'
+newId = 'New ID'
+newId_Inset = 'Insert a new ID:'
+newId_Insert_Back = 'Insert here'
+insert_new_ID = 'Insert new ID'
+noRoomDetected = 'No Room dected!'
+noIdDetected = 'No ID dected'
+negative_ID_message = 'Negatives ID are not possible!'
+id_not_number_error = 'ID is not a Number, please, try again!'
+id_sheet_error = 'No ID detected in the Sheet!'
+message_tutorial_addLocation = 'The following instructions have to be followed to import the\nlocations of Gossen Metrawatt databank:\n1) Create or rename location in the Gossen Metrawatt software \n2) Export the Data Base to a xml file \n3) The data musst be here imported.'
+processing = 'Processing...'
+done= 'Done!'
+operation_concluded = 'Operation Concluded!'
+operation_not_concluded = 'Operation not concluded!'
+file_not_supported = 'File not redable'
+backupDayMessage = "BackUP of the day"
+error_message_send_email = '--Outlook not installed--'
+Forgot_to_Save = 'Forgot to Save!'
+send_email_to_responsibles = 'Open e-mail sketch'
+save = 'Save'
+tableShowResultsHeader_NotVerified_1 = ["Select", "Building", "Floor", "Room", "Last Verification"]
+tableShowResultsHeader_NotVerified_2 = ["Building", "Floor", "Room", "E-Mail"]
+comboBoxData = ["Not Verified Rooms", "Verified Rooms"]
+messageInRed = 'There are no Rooms Available for the time Interval Selected'
+searchResponsible = "Search for Responsables"
+fromlabel = "From: "
+tolabel = "To: "
+untill = 'Untill: '
+choose_appointment = 'Choose appointment:'
+error_message_finding_responsibles = 'Problem detected! \nPlease review the URL'
+error_message_finding_responsibles_2 = 'Empty Table!'
+titleEmailSendToResponsibles = 'anstehende Elektrogeräteprüfung'
+notFound = 'Not Found'
+actualizeLink = "Actualize URL of the Institute's Employees"
+actualizeLink_title = 'Actualize URL'
+pleaseInsertURLhere = 'Please, insert the new URL:'
+saved = 'SAVED'
+undo = '&Undo Action'
+ImportWarningMessage = "Warning!\nAll data from Gossen Metrawatt Databank (Building, Room,\nDevices, Verification date) will be here imported and overwritten.\nHint: If you just want to import the locations go to:\n    Edit->Add locations"
+ExportWarningMessage = "Warning!\nAll information from the devices(Name, Manufacter, Serial\nnumber, Note) will be exported as Back-UP to the Gossen\nMetrawatt Databank. The verfication dates won't be exported.\nThis Back-UP have to be imported to the Gossen Metrawatt\ndatabank again."
+warning = 'Warning'
